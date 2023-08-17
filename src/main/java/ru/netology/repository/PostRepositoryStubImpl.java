1ad5cbb2a41a335545873e4fd9eecfedf9d69668
Anton Agafonnikov <feograf@mail.ru>
2023-08-17 20:55:18 +0300

    Add removed Posts Storage

--- src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
+++ src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -1,6 +1,7 @@
 package ru.netology.repository;
 
 import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.ArrayList;
@@ -12,23 +13,30 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class PostRepositoryStubImpl implements PostRepository {
+  // Открытое хранилище постов, доступное для просмотра и редактирования
   private final ConcurrentMap<Long, Post> posts = new ConcurrentHashMap<>();
+  // Хранилище удалённых постов, недоступное пользователю
+  private final ConcurrentMap<Long, Post> removedPostsStorage = new ConcurrentHashMap<>();
   private static final AtomicLong countPostID = new AtomicLong(0);
   public List<Post> all() {
-    return new ArrayList<>(posts.values());
+    return new ArrayList<>(new ArrayList<>(posts.values()));
   }
 
   public Optional<Post> getById(long id) {
     if(posts.containsKey(id)) {
       return Optional.ofNullable(posts.get(id));
     } else {
-      return Optional.empty();
+      throw new NotFoundException();
     }
   }
 
   public Post save(Post post) {
-    if(posts.containsKey(post.getId())) {
-      posts.put(post.getId(), post);
+    long id = post.getId();
+    if (removedPostsStorage.containsKey(id)) {
+      throw new NotFoundException();
+    }
+    else if (posts.containsKey(id)) {
+      posts.put(id, post);
     } else {
       post.setId(countPostID.incrementAndGet());
       posts.put(countPostID.get(), post);
@@ -37,8 +45,13 @@ public class PostRepositoryStubImpl implements PostRepository {
   }
 
   public void removeById(long id) {
+    // Если пост удаляется, то он помещается в закрытое хранилище
     if (posts.containsKey(id)) {
+      Post delPost = new Post(id, posts.get(id).getContent());
+      removedPostsStorage.put(id, delPost);
       posts.remove(id);
+    } else {
+      throw new NotFoundException();
     }
   }
 }
